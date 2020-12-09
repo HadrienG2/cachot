f929f0b5a4a14256b672f267dcbf65c1fcd8c579
Hadrien G <knights_of_ni@gmx.com>
2020-12-09 22:06:54 +0100

    Various code cleanup

---
+++ src/cache.rs
@@ -0,0 +1,93 @@
+//! Minimal cache simulator for 2D iteration locality studies
+
+use crate::FeedIdx;
+
+pub type Entry = FeedIdx;
+pub type Cost = f32;
+
+// Numbers stolen from the latency plot of Anandtech's Zen3 review, not very
+// precise but we only care about the orders of magnitude on recent CPUs...
+//
+// We're using numbers from the region where most of AnandTech's tests move out
+// of cache. The "full random" test is probably too pessimistic here.
+//
+// We're taking the height of cache latencies plateaux as our cost figure and
+// the abscissa of half-plateau as our capacity figure.
+//
+pub const L1_CAPACITY: usize = 32 * 1024;
+pub const L1_MISS_COST: Cost = 2.0;
+pub const L2_CAPACITY: usize = 512 * 1024;
+pub const L2_MISS_COST: Cost = 10.0;
+pub const L3_CAPACITY: usize = 32 * 1024 * 1024;
+pub const L3_MISS_COST: Cost = 60.0;
+
+/// CPU cache model, used for evaluating locality merits of 2D iteration schemes
+#[derive(Debug)]
+pub struct CacheModel {
+    // Entries ordered by access date, most recently accessed entry goes last
+    entries: Vec<Entry>,
+
+    // L1 capacity in entries
+    l1_entries: usize,
+
+    // L2 capacity in entries
+    l2_entries: usize,
+
+    // L3 capacity in entries
+    l3_entries: usize,
+}
+
+impl CacheModel {
+    /// Set up the cache model by telling the size of individual cache entries
+    pub fn new(entry_size: usize) -> Self {
+        Self {
+            entries: Vec::new(),
+            l1_entries: L1_CAPACITY / entry_size,
+            l2_entries: L2_CAPACITY / entry_size,
+            l3_entries: L3_CAPACITY / entry_size,
+        }
+    }
+
+    /// Tell how expensive it would be to access an entry (in units of L1 cache
+    /// miss costs, with L1 hits considered free), given how many other entries
+    /// have been accessed since the last time this entry was accessed.
+    fn cost_model(&self, age: usize) -> Cost {
+        if age < self.l1_entries {
+            0.0
+        } else if age < self.l2_entries {
+            1.0
+        } else if age < self.l3_entries {
+            L2_MISS_COST / L1_MISS_COST
+        } else {
+            L3_MISS_COST / L1_MISS_COST
+        }
+    }
+
+    /// Simulate loading an entry and return the cost in units of L1 cache miss
+    pub fn simulate_access(&mut self, entry: Entry) -> Cost {
+        // Look up the entry in the cache
+        let entry_pos = self.entries.iter().rposition(|&item| item == entry);
+
+        // Was it found?
+        if let Some(entry_pos) = entry_pos {
+            // If so, compute entry age and deduce access cost
+            let entry_age = self.entries.len() - entry_pos - 1;
+            let access_cost = self.cost_model(entry_age);
+
+            // Move the entry back to the front of the cache
+            self.entries.remove(entry_pos);
+            self.entries.push(entry);
+
+            // Return the access cost
+            access_cost
+        } else {
+            // If not, insert the entry in the cache
+            self.entries.push(entry);
+
+            // Report a zero cost. We don't want to penalize the first access in
+            // our cost model since it will have to happen no matter how good we
+            // are in our cache access pattern...
+            0.0
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,11 @@
+mod cache;
 mod morton;
+
+use crate::cache::CacheModel;
 
 use genawaiter::{stack::let_gen, yield_};
 
 type FeedIdx = usize;
-type Entry = FeedIdx;
-type Cost = f32;
-
-// Numbers stolen from the latency plot of Anandtech's Zen3 review, not very
-// precise but we only care about the orders of magnitude on recent CPUs...
-//
-// We're using numbers from the region where most of AnandTech's tests move out
-// of cache. The "full random" test is probably too pessimistic here.
-//
-// We're taking the height of cache latencies plateaux as our cost figure and
-// the abscissa of half-plateau as our capacity figure.
-//
-const L1_CAPACITY: usize = 32 * 1024;
-const L1_MISS_COST: Cost = 2.0;
-const L2_CAPACITY: usize = 512 * 1024;
-const L2_MISS_COST: Cost = 10.0;
-const L3_CAPACITY: usize = 32 * 1024 * 1024;
-const L3_MISS_COST: Cost = 60.0;
-
-/// CPU cache model, used for evaluating locality merits of 2D iteration schemes
-#[derive(Debug)]
-struct CacheModel {
-    // Entries ordered by access date, most recently accessed entry goes last
-    entries: Vec<Entry>,
-
-    // L1 capacity in entries
-    l1_entries: usize,
-
-    // L2 capacity in entries
-    l2_entries: usize,
-
-    // L3 capacity in entries
-    l3_entries: usize,
-}
-
-impl CacheModel {
-    /// Set up the cache model by telling the size of individual cache entries
-    pub fn new(entry_size: usize) -> Self {
-        Self {
-            entries: Vec::new(),
-            l1_entries: L1_CAPACITY / entry_size,
-            l2_entries: L2_CAPACITY / entry_size,
-            l3_entries: L3_CAPACITY / entry_size,
-        }
-    }
-
-    /// Tell how expensive it would be to access an entry (in units of L1 cache
-    /// miss costs, with L1 hits considered free), given how many other entries
-    /// have been accessed since the last time this entry was accessed.
-    fn cost_model(&self, age: usize) -> Cost {
-        if age < self.l1_entries {
-            0.0
-        } else if age < self.l2_entries {
-            1.0
-        } else if age < self.l3_entries {
-            L2_MISS_COST / L1_MISS_COST
-        } else {
-            L3_MISS_COST / L1_MISS_COST
-        }
-    }
-
-    /// Simulate loading an entry and return the cost in units of L1 cache miss
-    pub fn simulate_access(&mut self, entry: Entry) -> Cost {
-        // Look up the entry in the cache
-        let entry_pos = self.entries.iter().rposition(|&item| item == entry);
-
-        // Was it found?
-        if let Some(entry_pos) = entry_pos {
-            // If so, compute entry age and deduce access cost
-            let entry_age = self.entries.len() - entry_pos - 1;
-            let access_cost = self.cost_model(entry_age);
-
-            // Move the entry back to the front of the cache
-            self.entries.remove(entry_pos);
-            self.entries.push(entry);
-
-            // Return the access cost
-            access_cost
-        } else {
-            // If not, insert the entry in the cache
-            self.entries.push(entry);
-
-            // Report a zero cost. We don't want to penalize the first access in
-            // our cost model since it will have to happen no matter how good we
-            // are in our cache access pattern...
-            0.0
-        }
-    }
-}
 
 fn test_feed_pair_locality(
     debug_level: usize,
@@ -133,12 +47,12 @@ fn test_feed_pair_locality(
             "Total cache cost of iterator \"{}\" is {} ({:.2} per feed load)",
             name,
             total_cost,
-            total_cost / (feed_load_count as Cost)
+            total_cost / (feed_load_count as cache::Cost)
         ),
         _ => println!(
             "- Total cache cost of this iterator is {} ({:.2} per feed load)",
             total_cost,
-            total_cost / (feed_load_count as Cost)
+            total_cost / (feed_load_count as cache::Cost)
         ),
     }
 }
@@ -174,7 +88,7 @@ fn main() {
         // efficient to process.
         //
         for num_l1_entries in 3..num_feeds {
-            let entry_size = L1_CAPACITY / num_l1_entries;
+            let entry_size = cache::L1_CAPACITY / num_l1_entries;
             println!("--- Testing L1 capacity of {} feeds ---", num_l1_entries);
             if debug_level == 0 {
                 println!();
