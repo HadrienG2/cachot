4cb109cef5c239ab639eaf11d22c7f5828287ede
Hadrien G <knights_of_ni@gmx.com>
2021-01-18 14:31:22 +0100

    Préparation au brute force

--- src/main.rs
+++ src/main.rs
@@ -5,105 +5,6 @@ use crate::cache::CacheModel;
 use genawaiter::{stack::let_gen, yield_};
 use space_filler::{hilbert, morton, CurveIdx};
 
-/// Integer type used for counting radio feeds
-type FeedIdx = space_filler::Coordinate;
-
-/// Test harness for evaluating the locality of several feed pair iterators and
-/// picking the best of them.
-struct PairLocalityTester {
-    debug_level: usize,
-    entry_size: usize,
-    best_iterator: Option<(String, cache::Cost)>,
-}
-//
-impl PairLocalityTester {
-    /// Build the test harness
-    pub fn new(debug_level: usize, entry_size: usize) -> Self {
-        Self {
-            debug_level,
-            entry_size,
-            best_iterator: None,
-        }
-    }
-
-    /// Test the locality of one feed pair iterator
-    pub fn test_feed_pair_locality(
-        &mut self,
-        name: &str,
-        feed_pair_iterator: impl Iterator<Item = [FeedIdx; 2]>,
-    ) {
-        if self.debug_level > 0 {
-            println!("\nTesting feed pair iterator \"{}\"...", name);
-        }
-        let mut cache_model = CacheModel::new(self.entry_size);
-        let mut total_cost = 0.0;
-        let mut feed_load_count = 0;
-        for feed_pair in feed_pair_iterator {
-            if self.debug_level >= 2 {
-                println!("- Accessing feed pair {:?}...", feed_pair)
-            }
-            let mut pair_cost = 0.0;
-            for feed in feed_pair.iter().copied() {
-                let feed_cost = cache_model.simulate_access(feed);
-                if self.debug_level >= 2 {
-                    println!("  * Accessed feed {} for cache cost {}", feed, feed_cost)
-                }
-                pair_cost += feed_cost;
-            }
-            match self.debug_level {
-                0 => {}
-                1 => println!(
-                    "- Accessed feed pair {:?} for cache cost {}",
-                    feed_pair, pair_cost
-                ),
-                _ => println!("  * Total cache cost of this pair is {}", pair_cost),
-            }
-            total_cost += pair_cost;
-            feed_load_count += 2;
-        }
-        match self.debug_level {
-            0 => println!(
-                "Total cache cost of iterator \"{}\" is {} ({:.2} per feed load)",
-                name,
-                total_cost,
-                total_cost / (feed_load_count as cache::Cost)
-            ),
-            _ => println!(
-                "- Total cache cost of this iterator is {} ({:.2} per feed load)",
-                total_cost,
-                total_cost / (feed_load_count as cache::Cost)
-            ),
-        }
-        let best_cost = self
-            .best_iterator
-            .as_ref()
-            .map(|(_name, cost)| *cost)
-            .unwrap_or(cache::Cost::MAX);
-        if total_cost < best_cost {
-            self.best_iterator = Some((name.to_owned(), total_cost));
-        }
-    }
-
-    /// Tell which of the iterators that were tested so far got the best results
-    ///
-    /// If a tie occurs, pick the first iterator, as we're testing designs from
-    /// the simplest to the most complex ones.
-    ///
-    pub fn announce_best_iterator(&self) {
-        if self.debug_level > 0 {
-            println!();
-        }
-        let (best_name, best_cost) = self
-            .best_iterator
-            .as_ref()
-            .expect("No iterator has been tested yet");
-        println!(
-            "The best iterator so far is \"{}\" with cost {}",
-            best_name, best_cost
-        );
-    }
-}
-
 fn main() {
     #[rustfmt::skip]
     const TESTED_NUM_FEEDS: &'static [FeedIdx] = &[
@@ -188,11 +89,123 @@ fn main() {
             locality_tester.test_feed_pair_locality("Hilbert curve", hilbert);
 
             // Tell which iterator got the best results
-            locality_tester.announce_best_iterator();
+            let best_cost = locality_tester.announce_best_iterator();
+
+            // Now, let's try to brute-force a better iterator. First, evaluate
+            // all possible paths through the iteration domain where we don't
+            // step by more than (for now) [1, 1]...
+            let search_radius = 1;
+            let iteration_domain = (0..num_feeds)
+                .zip(0..num_feeds)
+                .map(|(feed1, feed2)| feed2 >= feed1)
+                .collect::<Vec<_>>();
+            /* fn find_iteration_paths(starting_point: [FeedIdx; 2], search_radius: FeedIdx, num_feeds: FeedIdx, reachable_positions: &[bool]) { */
 
             debug_level = debug_level.saturating_sub(1);
             println!();
         }
         debug_level = (num_feeds < 8).into();
+    }
+}
+
+// ---
+
+/// Integer type used for counting radio feeds
+type FeedIdx = space_filler::Coordinate;
+
+/// Test harness for evaluating the locality of several feed pair iterators and
+/// picking the best of them.
+struct PairLocalityTester {
+    debug_level: usize,
+    entry_size: usize,
+    best_iterator: Option<(String, cache::Cost)>,
+}
+//
+impl PairLocalityTester {
+    /// Build the test harness
+    pub fn new(debug_level: usize, entry_size: usize) -> Self {
+        Self {
+            debug_level,
+            entry_size,
+            best_iterator: None,
+        }
+    }
+
+    /// Test the locality of one feed pair iterator
+    pub fn test_feed_pair_locality(
+        &mut self,
+        name: &str,
+        feed_pair_iterator: impl Iterator<Item = [FeedIdx; 2]>,
+    ) {
+        if self.debug_level > 0 {
+            println!("\nTesting feed pair iterator \"{}\"...", name);
+        }
+        let mut cache_model = CacheModel::new(self.entry_size);
+        let mut total_cost = 0.0;
+        let mut feed_load_count = 0;
+        for feed_pair in feed_pair_iterator {
+            if self.debug_level >= 2 {
+                println!("- Accessing feed pair {:?}...", feed_pair)
+            }
+            let mut pair_cost = 0.0;
+            for feed in feed_pair.iter().copied() {
+                let feed_cost = cache_model.simulate_access(feed);
+                if self.debug_level >= 2 {
+                    println!("  * Accessed feed {} for cache cost {}", feed, feed_cost)
+                }
+                pair_cost += feed_cost;
+            }
+            match self.debug_level {
+                0 => {}
+                1 => println!(
+                    "- Accessed feed pair {:?} for cache cost {}",
+                    feed_pair, pair_cost
+                ),
+                _ => println!("  * Total cache cost of this pair is {}", pair_cost),
+            }
+            total_cost += pair_cost;
+            feed_load_count += 2;
+        }
+        match self.debug_level {
+            0 => println!(
+                "Total cache cost of iterator \"{}\" is {} ({:.2} per feed load)",
+                name,
+                total_cost,
+                total_cost / (feed_load_count as cache::Cost)
+            ),
+            _ => println!(
+                "- Total cache cost of this iterator is {} ({:.2} per feed load)",
+                total_cost,
+                total_cost / (feed_load_count as cache::Cost)
+            ),
+        }
+        let best_cost = self
+            .best_iterator
+            .as_ref()
+            .map(|(_name, cost)| *cost)
+            .unwrap_or(cache::Cost::MAX);
+        if total_cost < best_cost {
+            self.best_iterator = Some((name.to_owned(), total_cost));
+        }
+    }
+
+    /// Tell which of the iterators that were tested so far got the best results
+    ///
+    /// If a tie occurs, pick the first iterator, as we're testing designs from
+    /// the simplest to the most complex ones.
+    ///
+    pub fn announce_best_iterator(&self) -> cache::Cost {
+        if self.debug_level > 0 {
+            println!();
+        }
+        let (best_name, best_cost) = self
+            .best_iterator
+            .as_ref()
+            .expect("No iterator has been tested yet");
+        println!(
+            "The best iterator so far is \"{}\" with cost {}",
+            best_name, best_cost
+        );
+        *best_cost
     }
 }
